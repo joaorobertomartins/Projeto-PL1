e7a7da880074ecfe9957c316f35fb4f3e7089fba
joaomartins <joao.globaltecnologia@gmail.com>
2020-11-30 12:02:55 -0300

    Acrescentei um estudo de listas e estou tentando importar um código

---
+++ Estudo das Listas.py
@@ -0,0 +1,13 @@
+lista_1 = {1,5,10,50}
+
+print(lista_1)
+
+"Lista é uma estrutura linear - cresce apenas em uma dimensão"
+
+"Como declarar os elementos -  Abrindo couchetes e inserindo os elementos separados por vírgulas"
+
+"Como acessar os valores - Com o comando Print"
+
+
+
+
--- PLFAST.py
+++ PLFAST.py
@@ -1,6 +1,12 @@
 # Comentario
 import numpy as np
+from pulp import LpMaximize
+
 
 list_number = np.linspace(0, 100, 200)
 
 print(list_number)
+
+x = np.linspace(start=2, stop=4, num=10)
+
+
